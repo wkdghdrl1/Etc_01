1002ce294911bc7928dd6268764f3000c65cc200
wkdghdrl1 <power374@naver.com>
2019-06-14 16:48:43 +0900

    first

---
+++ src/com/biz/etc/Exec_02.java
@@ -0,0 +1,26 @@
+package com.biz.etc;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Exec_02 {
+public static void main(String[] args) {
+	
+
+	
+	List<String> strList = new ArrayList<String>();
+	
+	strList.add("홍길동");
+	strList.add("이몽룡");
+	strList.add("성춘향");
+	strList.add("임꺽정");
+	strList.add("양귀뷔");
+	
+	Collections.shuffle(strList);
+	
+	for(String s : strList) {
+		System.out.println(s);
+	}
+ 	}
+}
